e7ae618a5230c6d8e430d95f2bbf76e45314d45a
UloE-G <uloerumagborie@gmail.com>
2024-04-25 19:00:38 +0000

    Created all the variables for the game

--- run.py
+++ run.py
@@ -1,3 +1,109 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+import time
+
+# Global Variables
+grid = [[]]
+grid_size = 10
+ships = 4
+bullets = 20
+game_over = False
+sunk = 0
+ship_positions = [[]]
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def validate_ship(start_row, end_row, start_col, end_col):
+    """
+    Will check to see if it is a valid to place a ship in a specific row or column 
+    """
+    global grid
+    global ship_positions
+    
+    pass
+
+def help_place_ship(row, col, direction, lenght):
+    """
+    Based on the direction, will help to try and place ship on the grid
+    """
+    global grid_size
+
+    pass
+
+    return validate_ship(0, 0, 0, 0)
+
+def create_grid():
+    """
+    Will create a grid and randomly place down ships of 
+    different sizes in deiffernet directions.
+    """
+    global grid
+    global grid_size
+    global ships
+    global ship_positions
+
+    pass
+
+    help_place_ship(0, 0, 0, 0)
+
+def print_grid():
+    """
+    Prints out the grid
+    """
+
+    global grid
+    global letters
+
+    pass
+
+def bullet_placement():
+    """
+    Check if row and column the bullet is going is valid
+    """
+    global letters
+    global grid
+
+    pass
+
+    return 0, 0
+
+def ships_sunk(row, col):
+    """
+    When all parts of the ship have been hit,
+    the ship has sunk.
+    """
+    global ship_positions
+    global grid
+
+    pass
+
+def shot():
+    """
+    Updates grid and ship based on where the
+    bullet was shot.
+    """
+    global grid
+    global ships_sunk
+    global bullets
+
+    row, col = bullet_placement()
+
+    pass
+
+def game_done():
+    """
+    If all ships have sunk or no more bullets,
+    the game ends
+    """ 
+    global ships_sunk
+    global ships
+    global bullets
+    global game_over
+
+    pass
+
+def main():
+    """
+    Runs game loop
+    """ 
+    global  game_over
+
+    pass
