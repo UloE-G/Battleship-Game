f0a887d978a8fa824a263bd6b5d367b99d361273
UloE-G <uloerumagborie@gmail.com>
2024-05-01 17:16:38 +0000

    Changed some dialouge in bullter placement

--- run.py
+++ run.py
@@ -151,7 +151,7 @@ def bullet_placement():
     while is_valid_placement is False:
         placement = input("Enter row (A-J) and column (0-9) such as A3: \n")
         placement = placement.upper()
-        # If user enters to many numbers or letters print error
+        # If user enters to little or alot numbers or letters print error
         if len(placement) <= 0 or len(placement) > 2:
             print("Error: Please enter only one row and column such as A3")
             continue
